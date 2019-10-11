a1fd9be5b0800728a76697d66e0db59f382c5df8
[TakayukiNJ] <[nj.takayuki@gmail.com]>
2019-10-11 17:54:21 +0900

    https://github.com/musonza/chat-demo このでもを試した

--- config/app.php
+++ config/app.php
@@ -165,13 +165,14 @@ return [
         /*
          * Package Service Providers...
          */
+        Musonza\Chat\ChatServiceProvider::class,
 
         /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
@@ -191,7 +192,6 @@ return [
     'aliases' => [
 
         'App' => Illuminate\Support\Facades\App::class,
-        'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
@@ -221,11 +221,10 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Chat' => Musonza\Chat\Facades\ChatFacade::class,
     ],
 
 ];
---
+++ resources/js/components/ChatForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="input-group" v-if="isParticipant()">
+      <input
+        id="btn-input"
+        type="text"
+        name="message"
+        class="form-control input-sm"
+        placeholder="Type your message here..."
+        v-model="newMessage"
+        @keyup.enter="sendMessage"
+      >
+      &nbsp;&nbsp;
+      <span class="input-group-btn">
+        <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">Send</button>
+      </span>
+    </div>
+    <div v-else>
+      <button class="btn btn-success btn-sm" @click="joinConversation()">Join Conversation</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["user", "conversation"],
+
+  data() {
+    return {
+      newMessage: ""
+    };
+  },
+
+  methods: {
+    isParticipant() {
+      return window.conversations.indexOf(this.conversation) !== -1;
+    },
+
+    joinConversation() {
+      axios.post(`/conversations/${this.conversation}/users`).then(response => {
+        location.reload();
+      });
+    },
+
+    sendMessage() {
+      axios
+        .post(`/conversations/${this.conversation}/messages`, {
+          message: this.newMessage
+        })
+        .then(response => {
+          this.newMessage = "";
+          location.reload(); // comment this out if you are broadcasting
+        });
+    }
+  }
+};
+</script>
